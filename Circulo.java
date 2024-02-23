89162d4d85248d44558aced9eb2398a0e9752cd5
RicDioCar88 <rdiocar@g.educaand.es>
2024-02-23 12:45:29 +0100

    Añadido circulo y triangulo

---
+++ Circulo.java
@@ -0,0 +1,18 @@
+public class Circulo extends Figura{
+	private double radio;
+	
+	public Circulo(){
+	this.radio=radio;
+	}
+	
+	@Override
+	public double area(){
+	return Math.PI * radio * radio;
+	}
+	
+	@Override
+	public double perimetro(){
+	return 2*Math-PIradio;
+	}
+	
+}
